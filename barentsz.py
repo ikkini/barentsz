6d9a0267c4570f1b37293c9e3d2359b745041931
ikkini <ikkini@gmail.com>
2013-03-29 00:07:45 +0100

    IF NOT EXISTS (continue option)

--- barentsz.py
+++ barentsz.py
@@ -185,7 +185,7 @@ with con:
     cur = con.cursor()
 
     # ips should be unique
-    cur.execute("CREATE TABLE ips(ip STRING UNIQUE, alive BOOLEAN default 0, info STRING, ttl INT, type STRING, iperror INT default 0, seen INT default 0)")
+    cur.execute("CREATE TABLE IF NOT EXISTS ips(ip STRING UNIQUE, alive BOOLEAN default 0, info STRING, ttl INT, type STRING, iperror INT default 0, seen INT default 0)")
     # XXX with Timer():
     # if cb.targets_iter exists, it takes precendence
     if cb.targets_iter:
